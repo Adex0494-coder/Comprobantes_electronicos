43c4ad90832033b3f8e8a508c424100374b3e07e
Ariangel Díaz Espaillat <adex0494@gmail.com>
2021-05-05 17:16:12 -0400

    Signing method

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Serialization;
 
@@ -12,14 +14,28 @@ namespace Comprobantes_Electronicos
     {
         static async Task Main(string[] args)
         {
-            var restApi = new RestApi(GlobalConstants.testAutSemillaUrl);
-            await restApi.GetRequest();
+            //1)
+            //var restApi = new RestApi(GlobalConstants.testAutSemillaUrl);
+            //await restApi.GetRequest();
 
-            //Crear objeto semillaModel usando la respuesta al GetRequest, que tiene formato xml
-            var semillaModel = SemillaModel.XmlToObject(restApi.response);
+            ////Crear objeto semillaModel usando la respuesta al GetRequest, que tiene formato xml
+            //var semillaModel = SemillaModel.XmlToObject(restApi.response);
 
 
-            Console.WriteLine(semillaModel.valor + semillaModel.fecha);
+            //Console.WriteLine(semillaModel.valor + semillaModel.fecha);
+            //Console.ReadLine();
+
+            //2)
+            X509Certificate2 cert = new X509Certificate2(@"C:\Users\adiaz\Desktop\Ariangel\Work\ComprobantesElectrónicos\AriangelDazEspaillat-2021-05-05-085635.p12", "password", X509KeyStorageFlags.DefaultKeySet);
+            Console.WriteLine(cert);
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(@"C:\Users\adiaz\Desktop\Ariangel\Work\ComprobantesElectrónicos\response_1620235881248.xml");
+            xmlDoc.PreserveWhitespace = false;
+
+            var xmlSigned = new XmlSigned(xmlDoc, cert);
+            Console.WriteLine(xmlSigned.xmlDocument);
+
             Console.ReadLine();
         }
     }
---
+++ XmlSigned.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Comprobantes_Electronicos
+{
+    public class XmlSigned
+    {
+        public XmlDocument xmlDocument;
+        public X509Certificate2 cert;
+
+        public XmlSigned(XmlDocument xmlDocument, X509Certificate2 cert)
+        {
+            this.xmlDocument = xmlDocument;
+            this.cert = cert;
+            SignXmlWithP12();
+        }
+
+        private void SignXmlWithP12()
+        {
+
+            var privateKey = this.cert.PrivateKey;
+            var xmlSigned = new SignedXml(this.xmlDocument);
+            xmlSigned.SigningKey = privateKey;
+
+            var reference = new Reference();
+            reference.Uri = "";
+            var env = new XmlDsigEnvelopedSignatureTransform(true);
+            reference.AddTransform(env);
+
+            xmlSigned.AddReference(reference);
+
+            var keyInfo = new KeyInfo();
+            var clause = new KeyInfoX509Data();
+            clause.AddSubjectName(cert.Subject);
+            clause.AddCertificate(cert);
+            keyInfo.AddClause(clause);
+
+            xmlSigned.KeyInfo = keyInfo;
+            
+            xmlSigned.ComputeSignature();
+
+            XmlElement xmlDigitalSignature = xmlSigned.GetXml();
+
+            this.xmlDocument.DocumentElement.AppendChild(xmlDocument.ImportNode(xmlDigitalSignature, true));
+
+        }
+    }
+
+
+}
